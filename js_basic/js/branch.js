f4bf37ba3d58d523d7c2e69c6b326e1d93e47513
yourilwill <yourilwill.n.h@gmail.com>
2020-05-14 09:30:12 +0900

    if, if else tutorial.

---
+++ js_basic/js/branch.js
@@ -0,0 +1,18 @@
+// var x = 100;
+// var x = 99;
+// if (x >= 100) {
+//   console.log('100以上の値です');
+// } else {
+//   console.log('100未満の値です');
+// }
+
+var y = 59;
+if (y >= 90) {
+  console.log('A');
+} else if (y >= 80) {
+  console.log('B');
+} else if (y >= 60) {
+  console.log('C');
+} else {
+  console.log('D');
+}
