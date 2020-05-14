450bb934df9b8c9a778d3c2bca36372c41e8dbdd
yourilwill <yourilwill.n.h@gmail.com>
2020-05-14 09:15:27 +0900

    variable tutorial.

---
+++ js_basic/js/variable.js
@@ -0,0 +1,24 @@
+var x;
+
+// var i;
+// var j;
+var i,j;
+
+// var str = 'はじめてのJavaScript';
+var str = "はじめてのJavaScript";
+var k = 100;
+
+var name;
+var _name;
+var $name;
+
+var name1;
+var _name1;
+
+var foo;
+var Foo;
+
+// var , for
+
+var colorName = 'blue';
+console.log(colorName);
