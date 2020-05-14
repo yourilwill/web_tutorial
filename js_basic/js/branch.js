66be8ad1785075210b597d1d2a5fb4c9a805c1eb
yourilwill <yourilwill.n.h@gmail.com>
2020-05-14 09:34:13 +0900

    switch tutorial.

--- js_basic/js/branch.js
+++ js_basic/js/branch.js
@@ -6,13 +6,29 @@
 //   console.log('100未満の値です');
 // }
 
-var y = 59;
-if (y >= 90) {
-  console.log('A');
-} else if (y >= 80) {
-  console.log('B');
-} else if (y >= 60) {
-  console.log('C');
-} else {
-  console.log('D');
+// var y = 59;
+// if (y >= 90) {
+//   console.log('A');
+// } else if (y >= 80) {
+//   console.log('B');
+// } else if (y >= 60) {
+//   console.log('C');
+// } else {
+//   console.log('D');
+// }
+
+var n = 4;
+switch (n) {
+  case 1:
+    console.log('数値は1です');
+    break;
+  case 2:
+    console.log('数値は2です');
+    break
+  case 3:
+    console.log('数値は3です');
+    break;
+  default:
+    console.log('範囲外です');
+    break;
 }
