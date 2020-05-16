242d8e3f4c4b2b6a11d09ba36c66078fe73161ab
yourilwill <yourilwill.n.h@gmail.com>
2020-05-16 18:23:41 +0900

    function order and literal tutorial.

---
+++ js_basic/js/function.js
@@ -0,0 +1,17 @@
+// function命令
+// function getRectangle(height, width){
+//   return height * width;
+// }
+
+// console.log(getRectangle(3, 5));
+
+// 関数リテラル
+// var getRectangle = function(height, width){
+//   return height * width
+// };
+
+// console.log(getRectangle(3, 5));
+
+// Functionコンストラクタ
+
+
