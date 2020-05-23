12f66bb448953332c359d78fee28a78d4dad1fac
yourilwill <yourilwill.n.h@gmail.com>
2020-05-23 19:41:37 +0900

    make initialize_lecture_select.sql.

---
+++ mysql/docker/db/initialize_lecture_select.sql
@@ -0,0 +1,17 @@
+drop database if exists mydb;
+create database mydb;
+grant all on mydb.* to mydbuser@localhost identified by 'password';
+
+use mydb;
+create table users (
+  id int unsigned auto_increment not null primary key,
+  name varchar(32),
+  age int
+);
+insert into users(name, age) values('sato', 18);
+insert into users(name, age) values('suzuki', 22);
+insert into users(name, age) values('takahashi', 29);
+insert into users(name, age) values('tanaka', 30);
+insert into users(name, age) values('ito', 19);
+insert into users(name, age) values('watanabe', 20);
+insert into users(name, age) values('yamamoto', null);
